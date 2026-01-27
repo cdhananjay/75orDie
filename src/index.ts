fb2589b0472e1a05ef86d2dbb37fc8036cd85610
Dhananjay Chavan <219833325+cdhananjay@users.noreply.github.com>
2026-01-27 19:47:07 +0530

    add auth controllers

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,24 @@ import cookieParser from 'cookie-parser';
 import authRouter from "./routes/auth.routes.js";
 import subjectRouter from "./routes/subject.routes.js";
 import { drizzle } from 'drizzle-orm/neon-http';
+import {usersTable} from "./db/schema.js";
 
 const app = express()
 const port = 3000
 
 const db = drizzle(process.env.DATABASE_URL!);
+
+(async ()=> {
+    try {
+        const users = await db.select().from(usersTable);
+        console.log(users);
+        console.log("DB CONNECTED")
+    } catch (e) {
+        if (e instanceof Error) console.error(e);
+        console.log("ERROR CONNECTING TO DB");
+        process.exit(1);
+    }
+})()
 
 app.use(cookieParser())
 app.use(express.json())
@@ -15,9 +28,10 @@ app.use('/api/auth' , authRouter);
 app.use('/api/sub' , subjectRouter);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send("hello world");
+})
+app.listen(port, () => {
+    console.log(`http://localhost:${port}/`)
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+export default db;
